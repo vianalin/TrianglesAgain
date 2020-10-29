789826041b6e48f44ccc74c94b8492f46f6296f0
vianalin <vlin21@stuy.edu>
2020-10-29 17:35:24 -0400

    tested area

--- Tester.java
+++ Tester.java
@@ -3,6 +3,7 @@ public class Tester {
 		Point a = new Point(0, 0);
 		Triangle b = new Triangle(new Point(1, 2), new Point(3, 4), new Point(6, 7)); 
 		Triangle c = new Triangle(1, 2, 3, 4, 6, 7);
+		Triangle d = new Triangle(2, 6, 4, 8, 56, 80);
 
 		//2.3
 		System.out.println(a.distanceTo(new Point(2, 2))); // a; testing distance 
@@ -12,6 +13,7 @@ public class Tester {
 		//2.4 c
 		System.out.println(b.getPerimeter()); //14.1421
 		System.out.println(c.getPerimeter()); //testing perimeter & both constructors
+		System.out.println(d.getArea()); //20 testing area
 	}
 	
 }
