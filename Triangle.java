1b0dcafd71923fd497830a93feaf9e6733c3c8d5
vianalin <vlin21@stuy.edu>
2020-10-29 17:11:17 -0400

    2.4 a & b

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle{
+	private Point v1, v2, v3; //a
+
+	public Triangle(Point a, Point b, Point c) { //b
+		v1 = a;
+		v2 = b;
+		v3 = c;
+	}
+
+	public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) { //b
+		v1 = new Point(x1, y1);
+		v2 = new Point(x2, y2);
+		v3 = new Point(x3, y3);
+	}
+
+
+}
