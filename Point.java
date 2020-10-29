004317ea7ed2ce5064436b7af9e99763bc184f73
vianalin <vlin21@stuy.edu>
2020-10-29 16:58:41 -0400

    2.3 distanceto

--- Point.java
+++ Point.java
@@ -1,3 +1,30 @@
 public class Point {
-	
+	//2.2
+	private double x, y; 
+
+	public Point(double X, double Y) { //construct a point given coordinates
+		x = X;
+		y = Y;
+	}
+
+	public Point(Point p) { //construct point given a point (redundant, gives flexibility)
+		x = p.x; //priv access in same file is allowed
+		y = p.y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	//2.3
+	public double distanceTo(Point other) { //a
+		return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+	}
+
+
+
 }
