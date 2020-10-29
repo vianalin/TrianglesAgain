f63a0c53917ea52928166c1b27c09a53ba4cfc79
vianalin <vlin21@stuy.edu>
2020-10-29 17:03:44 -0400

    tested equals

--- Tester.java
+++ Tester.java
@@ -1,7 +1,11 @@
 public class Tester {
 	public static void main(String[] args) {
 		Point a = new Point(0, 0);
-		System.out.println(a.distanceTo(new Point(2, 2))); //testing distance
+
+		//2.3
+		System.out.println(a.distanceTo(new Point(2, 2))); // a; testing distance 
+		System.out.println(a.equals(new Point(0, 0))); // b; testing equals true
+		System.out.println(a.equals(new Point(-1, 55))); // b; testing equals false
 	}
 	
 }
